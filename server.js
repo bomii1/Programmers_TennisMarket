c48b7bf96cc6d0ce6eeac5347013467e500d7c0b
bomii1 <202103076@hufs.ac.kr>
2024-08-23 21:53:17 +0900

    💄 CSS수정, 상품 Card 반응형으로 고침
    flex-basis 사용, 상품 Card 이미지 반응형으로 고침

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ function start(route, handle) {
     function onRequest(request, response) {
         let pathname = url.parse(request.url).pathname; // 리퀘스트 url 의 경로를 확인
         let queryData = url.parse(request.url, true).query;
-        // 쿼리데이터는 
+        
         if (pathname === '/favicon.ico') {
             response.writeHead(200, { 'Content-Type': 'image/x-icon' });
             return response.end();
